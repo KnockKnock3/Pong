705b48bb81bee18ae16cda26977f58122e1333b0
KnockKnock3 <knockknock3offical@gmail.com>
2024-09-29 00:11:17 +0100

    Hello World Window

--- main.c
+++ main.c
@@ -1,6 +1,29 @@
+#include "raylib.h"
 #include <stdio.h>
 
+#define WINDOW_WIDTH 1600
+#define WINDOW_HEIGHT 900
+
 int main() {
-    printf("Hello World!\n");
+
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Pong");
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+
+        float deltaTime = GetFrameTime();
+
+        BeginDrawing();
+
+            ClearBackground(BLACK);
+
+            DrawFPS(0, 0);
+            DrawText("Hello World", 100, 100, 20, RED);
+
+        EndDrawing();
+
+    }
+
     return 0;
 }
